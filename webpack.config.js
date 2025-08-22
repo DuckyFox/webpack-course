511738e3be7cc7c3d13228ea256ce781ef7d0962
ВашÐDima <diman.yackim@yandex.ru>
2025-08-22 14:47:52 +0400

    installed html webpack and progress bar

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path')
+const webpack = require('webpack')
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = (env) => {
     return {
         mode: env.mode,
         entry: path.resolve(__dirname, 'src', 'index.js'),
+        plugins: [
+            new htmlWebpackPlugin({
+                title: "Webpack Application",
+                template: path.resolve(__dirname, 'public/index.html'),
+            }),
+            new webpack.ProgressPlugin(),
+        ],
         output: {
             path: path.resolve(__dirname, 'build'),
             filename: '[name].[contenthash].js',
